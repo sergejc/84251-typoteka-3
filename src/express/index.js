550362d7f1882c676c4a350b2dcf32f869ea7a9c
Sergej Charskij <sergej.charskij@gmail.com>
2020-02-24 19:39:52 +0000

    Update routes file

--- src/express/index.js
+++ src/express/index.js
@@ -1,19 +1,12 @@
 'use strict';
 
 const express = require(`express`);
-const {myRoutes, offerRoutes} = require(`./routes`);
+const routes = require(`./routes`);
 
 const DEFAULT_PORT = 8080;
 
 const app = express();
-
-app.use(`/my`, myRoutes);
-app.use(`/offers`, offerRoutes);
-
-app.get(`/register`, (req, res) => res.send(`/register`));
-app.get(`/login`, (req, res) => res.send(`/login`));
-app.get(`/search`, (req, res) => res.send(`/search`));
-app.get(`/`, (req, res) => res.send(`/`));
+app.use(routes);
 
 
 app.listen(DEFAULT_PORT,
--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -3,9 +3,9 @@
 const {Router} = require(`express`);
 const myOffers = new Router();
 
-myOffers.get(`/category/:id`, (req, res) => res.send(`/offers/category/:id`));
+myOffers.get(`/category/:id`, (req, res) => res.send(`/offers/category/${req.params.id}`));
 myOffers.get(`/add`, (req, res) => res.send(`/offers/add`));
-myOffers.get(`/edit/:id`, (req, res) => res.send(`offers/edit/:id`));
-myOffers.get(`/:id`, (req, res) => res.send(`offers/:id`));
+myOffers.get(`/edit/:id`, (req, res) => res.send(`offers/edit/${req.params.id}`));
+myOffers.get(`/:id`, (req, res) => res.send(`offers/${req.params.id}`));
 
 module.exports = myOffers;
