bd0b022fe628dd9449060b5711d1b60d4800547d
Sergej Charskij <sergej.charskij@gmail.com>
2020-03-01 10:51:35 +0000

    Add page templates

--- src/express/index.js
+++ src/express/index.js
@@ -2,10 +2,14 @@
 
 const express = require(`express`);
 const routes = require(`./routes`);
+const path = require(`path`);
 
 const DEFAULT_PORT = 8080;
 
 const app = express();
+app.use(express.static(`markup`));
+app.set(`views`, path.join(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 app.use(routes);
 
 
