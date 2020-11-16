4d56a0664e618542e6e7ef2185be22db66fda4ce
Sergej Charskij <sergej.charskij@gmail.com>
2020-11-16 20:14:49 +0000

    Add file logger

--- src/api/logger.js
+++ src/api/logger.js
@@ -1,9 +1,10 @@
 'use strict';
+const pino = require(`pino`);
 
-const logger = require(`pino`)({
+const logger = pino({
   name: `pino-and-express`,
   level: process.env.LOG_LEVEL || `info`
-});
+}, pino.destination(`./src/service/logs/output.log`));
 
 module.exports = {
   logger,
