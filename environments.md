c920dcbfb4120f9a6ec7673ed2f89a88ea3a0cbe
Sergej Charskij <sergej.charskij@gmail.com>
2021-06-07 21:09:45 +0100

    Add sequelize

---
+++ environments.md
@@ -0,0 +1,7 @@
+## СУБД
+
+`DB_HOST` — host;
+`DB_PORT` - port number;
+`DB_USER` — user name;
+`DB_NAME` - database name;
+`DB_PASSWORD` - password;
--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,14 @@
 const app = require(`./app`);
 const {logger} = require(`./logger`);
 const constants = require(`../constants`);
+const {connectDb} = require(`../api/services/db`);
 
 const DEFAULT_PORT = 3000;
 const port = Number.parseInt(process.argv[2], 10) || DEFAULT_PORT;
 
 (async () => {
   try {
+    await connectDb();
     const server = await app();
     server.listen(port, () => {
       logger.info(`Server start on port: ${port}`);
